787a20d4ad3d84ce3bb8445bdc04e22cc137486b
dockit0 <47304313+dockit0@users.noreply.github.com>
2019-03-06 20:28:20 -0500

    add Conditionals exercises 1-10

---
+++ Conditionals/cond1ex.rb
@@ -0,0 +1,8 @@
+# Write an if statement that prints "The sun is so bright!" if sun equals 'visible'.
+
+sun = ['visible', 'hidden'].sample
+
+if sun == 'visible'
+  puts 'The sun is so bright!'
+end
+
