d614a316ef51ad5a46ade12d049c3f5c6d68a0e7
dockit0 <47304313+dockit0@users.noreply.github.com>
2019-03-06 20:27:51 -0500

    add Methods exercises 1-10

---
+++ Methods/methods4ex.rb
@@ -0,0 +1,18 @@
+# Write a method named greet that invokes the following methods:
+
+def hello
+  'Hello'
+end
+
+def world
+  'World'
+end
+
+
+# answer ++++++++++++++=
+
+def greet
+  hello + ' ' + world
+end
+
+puts greet
