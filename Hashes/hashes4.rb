6d25b17254207d42ef3b43337fb90e295d432501
dockit0 <47304313+dockit0@users.noreply.github.com>
2019-03-08 15:27:11 -0500

    add Hashes exercises 1-10

---
+++ Hashes/hashes4.rb
@@ -0,0 +1,15 @@
+# Using the following code, select the value 'blue' from car and print it with #puts.
+
+car = {
+  type:    'sedan',
+  color:   'blue',
+  year:    2003
+}
+
+
+# answer: 
+
+car[:type]  # "sedan"
+car[:color] # "blue"
+car[:year]  # 2003
+
---
+++ Hashes/hashes9.rb
@@ -0,0 +1,15 @@
+# Create a nested hash using the following data.
+
+# CAR type: sedan, color: blue, year: 2003
+# TRUCK type: pickup, color: red, year: 1998
+
+
+# answer: 
+
+ {
+  car:   { type: 'sedan', color: 'blue', year: 2003 },
+  truck: { type: 'pickup', color: 'red', year: 1998 }
+}
+
+
+
